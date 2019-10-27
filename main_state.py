5c487448617ef473b0379c9e1e847a242bc55a4b
SuperHong94 <50072558+SuperHong94@users.noreply.github.com>
2019-10-27 16:01:48 +0900

    8방향 구현
    굿굿굿

--- main_state.py
+++ main_state.py
@@ -30,18 +30,43 @@ class Player:
         self.frame=2  # 0이left, 2==정지,위,아래 , 4==오른쪽
         self.direction='stop'
         self.speed=1
+        self.multiKey={'up':False,'down':False,'left':False,'right':False}
         pass
 
     def update(self):
         if(self.direction=="stop"):
             pass
         elif self.direction== 'left':
+            if self.multiKey['up']==True:
+                self.y+=self.speed
+            elif self.multiKey['down']==True:
+                self.y -= self.speed
+            # elif self.multiKey['left']==True:
+            #     self.x -= self.speed
             self.x-=self.speed
         elif self.direction== 'up':
+            if self.multiKey['left'] == True:
+                self.x -= self.speed
+            elif self.multiKey['right'] == True:
+                self.x += self.speed
+            # elif self.multiKey['up']==True:
+            #     self.y += self.speed
             self.y+=self.speed
         elif self.direction== 'down':
+            if self.multiKey['left'] == True:
+                self.x -= self.speed
+            elif self.multiKey['right'] == True:
+                self.x += self.speed
+            # elif self.multiKey['down']==True:
+            #     self.y -= self.speed
             self.y-=self.speed
         elif self.direction== 'right':
+            if self.multiKey['up']==True:
+                self.y+=self.speed
+            elif self.multiKey['down']==True:
+                self.y -= self.speed
+            # elif self.multiKey['right'] == True:
+            #     self.x += self.speed
             self.x+=self.speed
         pass
 
@@ -101,23 +126,37 @@ def handle_events():
         if event.type==SDL_QUIT:
             game_framework.quit()
         elif event.type==SDL_KEYDOWN and event.key==SDLK_ESCAPE:
-            print("ahffk")
             #game_framework.change_state(title_state)
+            pass
         elif event.type==SDL_KEYDOWN and event.key==SDLK_p:
             game_framework.push_state(pause_sate)
         elif event.type==SDL_KEYDOWN and event.key==SDLK_LEFT:
             player.frame=0
+            player.multiKey['left']=True
             player.direction='left'
         elif event.type==SDL_KEYDOWN and event.key==SDLK_UP:
             player.frame = 2
+            player.multiKey['up'] = True
             player.direction='up'
         elif event.type==SDL_KEYDOWN and event.key==SDLK_DOWN:
             player.frame = 2
+            player.multiKey['down'] = True
             player.direction='down'
         elif event.type==SDL_KEYDOWN and event.key==SDLK_RIGHT:
             player.frame = 4
+            player.multiKey['right'] = True
             player.direction='right'
         elif event.type==SDL_KEYUP and (event.key==SDLK_LEFT or event.key==SDLK_UP or event.key==SDLK_DOWN or event.key==SDLK_RIGHT):
+            print(event.key)
+            if event.key==SDLK_UP:
+                player.multiKey['up'] = False
+            elif event.key==SDLK_LEFT:
+                player.multiKey['left'] = False
+            elif event.key==SDLK_RIGHT:
+                player.multiKey['right'] = False
+            elif event.key==SDLK_DOWN:
+                player.multiKey['down'] = False
+
             player.frame = 2
             player.direction='stop'
         if event.type == SDL_KEYDOWN and event.key == SDLK_a:
