b98099c6a9e423e94f149476617d5ffc5d2387f7
SuperHong94 <50072558+SuperHong94@users.noreply.github.com>
2019-10-29 21:33:50 +0900

    새로운 적 타입 추가
    ..

--- enemis.py
+++ enemis.py
@@ -8,7 +8,7 @@ class Enemy:
     image=None
     bulletImage=None
     def __init__(self):
-        self.enemyType=random.randint(0,1)
+        self.enemyType=random.randint(0,2)
         if Enemy.bulletImage==None:
             Enemy.bulletImage=load_image('resource/enemy_bullet.png')
 
@@ -16,7 +16,9 @@ class Enemy:
             self.image = load_image('resource/enemy0.png')
         elif self.enemyType==1:
             self.image=load_image('resource/enemy1.png')
-        self.x, self.y = random.randint(50,800), random.randint(800,1000)
+        elif self.enemyType==2:
+            self.image=load_image('resource/enemy2.png')
+        self.x, self.y = random.randint(50,800), random.randint(800,2000)
         self.bX,self.bY=self.x,self.y-10
         self.frame = 0  # 0이left, 2==정지,위,아래 , 4==오른쪽
         self.speed = 0.5
@@ -38,16 +40,14 @@ class Enemy:
 
     def draw(self):
         global Timer
-        self.image.clip_draw(30*self.frame,0,30,30,self.x,self.y)
+        self.image.clip_draw(30 * self.frame, 0, 30, 30, self.x, self.y)
         if(self.fire):
             if self.enemyType==0:
                 self.timer+=1
                 t = self.timer / 500
                 self.bX = (1 - t) * self.sTargetX + t * self.targetX
                 self.bY = (1 - t) * self.sTargetY + t * self.targetY
-
                 self.bulletImage.clip_draw(0, 0, 10, 10, self.bX, self.bY)
-
             elif self.enemyType==1:
                 self.timer += 1
                 #cX=((self.targetX-self.sTargetX)**2)**0.5
@@ -61,13 +61,20 @@ class Enemy:
                 else:
                     i=-1
 
-                print(i*dx)
-
                 t = self.timer / 500
                 self.bX = ((1 - t)**3) * self.sTargetX +3*(cX+(i*dx))*t*((1-t)**2)+3*(cX+(i*(-1)*dx1))*(t**2)*(1-t)+ (t**3) * self.targetX
                 self.bY = ((1 - t)**3) * self.sTargetY + 3 * (cY * 0.25) * t * ((1 - t) ** 2) + 3 * (cY * 0.5) * (t ** 2) * (1 - t) + (t ** 3) * self.targetY
+                self.bulletImage.clip_draw(0, 0, 10, 10, self.bX, self.bY)
+            elif self.enemyType==2:
+                self.timer+=1
+                t = self.timer / 500
+                self.x = (1 - t) * self.sTargetX + t * self.targetX
+                self.y = (1 - t) * self.sTargetY + t * self.targetY
 
-                self.bulletImage.clip_draw(0,0,10,10,self.bX,self.bY)
+
+
+
+
 
         pass
 
