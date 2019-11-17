969500ba36c62aab9e2953622aad193fae422e05
SuperHong94 <50072558+SuperHong94@users.noreply.github.com>
2019-11-17 15:55:23 +0900

    좌측 우측키 이벤트 추가
    ..

--- origin/player.py
+++ origin/player.py
@@ -1,37 +1,88 @@
-
 from pico2d import *
 import game_framework
-#Player Event
-RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP=range(4)
 
-key_event_table={
-    (SDL_KEYDOWN,SDLK_RIGHT):RIGHT_DOWN,
-    (SDL_KEYDOWN,SDLK_LEFT):LEFT_DOWN,
-    (SDL_KEYUP,SDLK_RIGHT):RIGHT_UP,
-    (SDL_KEYUP,SDLK_LEFT):LEFT_UP
+# Player Event
+RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP = range(4)
+
+key_event_table = {
+    (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
+    (SDL_KEYDOWN, SDLK_LEFT): LEFT_DOWN,
+    (SDL_KEYUP, SDLK_RIGHT): RIGHT_UP,
+    (SDL_KEYUP, SDLK_LEFT): LEFT_UP
 }
+
+
 class FlyState:
     @staticmethod
-    def enter(player,event):
-        if event==RIGHT_DOWN:
+    def enter(player, event):
+        if event == RIGHT_DOWN:
+            player.velocity += 1
+        elif event == LEFT_DOWN:
+            player.velocity -= 1
+        elif event == RIGHT_UP:
+            player.velocity -= 1
+        elif event == LEFT_UP:
+            player.velocity += 1
+        player.dir = player.velocity
+
+    @staticmethod
+    def exit(player, event):
+        pass
+
+    @staticmethod
+    def do(player):
+        if player.dir < 0:
+            player.frame = 0
+        elif player.dir > 0:
+            player.frame = 4
+        elif player.dir == 0:
+            player.frame = 2
+        player.x += player.velocity
+        player.x = clamp(25, player.x, 800 - 25)
+
+    @staticmethod
+    def draw(player):
+        player.image.clip_draw(30 * player.frame, 0, 30, 30, player.x, player.y)
+
+
+next_state_table = {
+    FlyState: {RIGHT_UP: FlyState, LEFT_UP: FlyState,
+               RIGHT_DOWN: FlyState, RIGHT_UP: FlyState}
+}
 
 
 class Player:
     image = None
-    def __init__(self):
 
+    def __init__(self):
+        if Player.image == None:
+            Player.image = load_image('resource/player(edit).png')
         self.x, self.y = 400, 10
-        self.frame=2  # 0이left, 2==정지,위,아래 , 4==오른쪽
-        self.direction='stop'
-        self.velocity=1
-        self.multiKey={'up':False,'down':False,'left':False,'right':False}
-        if Player.image ==None:
-            Player.image=load_image('resource/player(edit).png')
+        self.frame = 2  # 0이left, 2==정지,위,아래 , 4==오른쪽
+        self.dir = 0
+        self.velocity = 1
+        self.event_que=[]
+        self.cur_state=FlyState
+        self.cur_state.enter(self,None)
         pass
+
+    def add_event(self, event):
+        self.event_que.insert(0, event)
 
     def update(self):
+        self.cur_state.do(self)
+        if len(self.event_que) > 0:
+            event = self.event_que.pop()
+            self.cur_state.exit(self.event)
+            self.cur_state = next_state_table[self.cur_state][event]
+            self.cur_state.enter(self, event)
         pass
 
+
     def draw(self):
-        Player.image.clip_draw(30*self.frame,0,30,30,self.x,self.y)
+        self.cur_state.draw(self)
         pass
+    def handle_event(self,event):
+        if(event.type,event.key) in key_event_table:
+            key_event=key_event_table[(event.type,event.key)]
+            self.add_event(key_event)
