2fbdcc298c4e85de5a2584860d40bfdd78540f47
SuperHong94 <50072558+SuperHong94@users.noreply.github.com>
2019-11-17 15:10:57 +0900

    코드 싹갈아 엎는중 1
    ..

--- origin/player.py
+++ origin/player.py
@@ -1,5 +1,6 @@
-from pico2d import *
 
+from pico2d import *
+import game_framework
 #Player Event
 RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP=range(4)
 
@@ -10,6 +11,10 @@ key_event_table={
     (SDL_KEYUP,SDLK_LEFT):LEFT_UP
 }
 class FlyState:
+    @staticmethod
+    def enter(player,event):
+        if event==RIGHT_DOWN:
+
 
 class Player:
     image = None
@@ -18,47 +23,13 @@ class Player:
         self.x, self.y = 400, 10
         self.frame=2  # 0이left, 2==정지,위,아래 , 4==오른쪽
         self.direction='stop'
-        self.speed=1
+        self.velocity=1
         self.multiKey={'up':False,'down':False,'left':False,'right':False}
         if Player.image ==None:
             Player.image=load_image('resource/player(edit).png')
         pass
 
     def update(self):
-        if(self.direction=="stop"):
-            pass
-        elif self.direction== 'left':
-            if self.multiKey['up']==True:
-                self.y+=self.speed
-            elif self.multiKey['down']==True:
-                self.y -= self.speed
-            # elif self.multiKey['left']==True:
-            #     self.x -= self.speed
-            self.x-=self.speed
-        elif self.direction== 'up':
-            if self.multiKey['left'] == True:
-                self.x -= self.speed
-            elif self.multiKey['right'] == True:
-                self.x += self.speed
-            # elif self.multiKey['up']==True:
-            #     self.y += self.speed
-            self.y+=self.speed
-        elif self.direction== 'down':
-            if self.multiKey['left'] == True:
-                self.x -= self.speed
-            elif self.multiKey['right'] == True:
-                self.x += self.speed
-            # elif self.multiKey['down']==True:
-            #     self.y -= self.speed
-            self.y-=self.speed
-        elif self.direction== 'right':
-            if self.multiKey['up']==True:
-                self.y+=self.speed
-            elif self.multiKey['down']==True:
-                self.y -= self.speed
-            # elif self.multiKey['right'] == True:
-            #     self.x += self.speed
-            self.x+=self.speed
         pass
 
     def draw(self):
